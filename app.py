018564829ff7afd8c4de1dedba6396a65493ea57
Rhys Flook <rhys.flook@googlemail.com>
2022-02-08 15:13:35 +0900

    create working implementation for sending player moves

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import asyncio
 import json
 import websockets
+import secrets
 
+JOIN = {}
 
-async def handler(websocket):
-    
+async def send_messages(websocket, connected):
+
     async for message in websocket:
         event = json.loads(message)
-        await websocket.send(json.dumps(event))
-        print(message)
+        websockets.broadcast(connected, json.dumps(event))
     # while True:
     #     try:
     #         message = await websocket.recv()
@@ -16,11 +17,45 @@ async def handler(websocket):
     #         break
     #     print(message)
 
+async def start(websocket, token):
+    connected = {websocket}
+    print(token)
+    JOIN[token] = connected
+    print(JOIN)
+    try:
+        await send_messages(websocket, connected)
+    finally:
+        del JOIN[token]
+
+async def join(websocket, token):
+    print(JOIN)
+    print(token)
+    connected = JOIN[token]
+    connected.add(websocket)
+    try:
+        await send_messages(websocket, connected)
+    finally:
+        connected.remove()
 
 async def main():
     async with websockets.serve(handler, "", 8001):
         await asyncio.Future()  # run forever
 
+async def handler(websocket, path):
+    """
+    Handle a connection and dispatch it according to who is connecting.
+
+    """
+    # Receive and parse the "init" event from the UI.
+    message = await websocket.recv()
+    event = json.loads(message)
+
+    if event['type'] == 'join':
+        # Second player joins an existing game.
+        await join(websocket, event['token'])
+    else:
+        # First player starts a new game.
+        await start(websocket, event['id'])
 
 if __name__ == "__main__":
     asyncio.run(main())
--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const websocket = new WebSocket('ws://localhost:8001/');
-const butt = document.querySelector('#button');
-const message = document.querySelector('#message');
+const butt = document.querySelector('#butt');
+const fromX = document.querySelector('#fx');
+const fromY = document.querySelector('#fy');
+const toX = document.querySelector('#tx');
+const toY = document.querySelector('#ty');
+const capturing = document.querySelector('#capturing');
+
+setTimeout(() => websocket.send(JSON.stringify({type: 'join', token: '2'})), 500);
+
+const reverse = (value) => {
+  return Math.abs(Number(value) - 7);
+}
 
 butt.addEventListener('click', () => {
-  const event = { type: 'message', content: message.value };
+  const message = {from: {x: reverse(fx.value), y: reverse(fy.value)}, to: {x: reverse(tx.value), y: reverse(ty.value)}, capturing: capturing.checked, colour: "black"}
+  const event = { type: 'move', content: JSON.stringify(message) };
+  // console.log(message)
   websocket.send(JSON.stringify(event));
 });
 
