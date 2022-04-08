444da92d318a42177dddc7ad9d09c25c6e95a318
Rhys Flook <rhys.flook@googlemail.com>
2022-04-08 15:24:35 +0900

    update friend functionality so friends are notified of login and logout events

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import secrets
 import os
 import signal
 
+import requests
 
 class Connection:
     def __init__(self, id, websocket):
@@ -13,6 +14,71 @@ class Connection:
         self.websocket = websocket
         self.in_game = False
         self.opponent = None
+        self.active = True
+    
+    def get_friends(self): 
+
+        friend_data = requests.get(f'https://dragon-king-games.herokuapp.com/main-menu.php/draughts/backend/friends/getFriends.php?id={self.id}').json()
+        friends = {}
+        for friend in friend_data:
+            id, name = friend[0], friend[1]
+            friends[id] = {
+                'id': id,
+                'name': name,
+                'online': False,
+                'inGame': False
+            }
+            if id in JOIN.keys():
+                friends[id]['online'] = True
+                friends[id]['inGame'] = JOIN[id].in_game
+        
+        return friends
+    
+    def get_friends_sockets(self):
+        friend_data = requests.get(f'http://localhost/draughts/backend/friends/getFriends.php?id={self.id}').json()
+        return [JOIN[friend[0]].websocket for friend in friend_data if friend[0] in JOIN.keys()]
+
+    def handle_login(self):
+        friends = self.get_friends_sockets()
+        print(friends)
+        websockets.broadcast(self.get_friends_sockets(), json.dumps({
+            'type': 'login',
+            'id': self.id
+        }))
+
+    def notify_opponent(self, is_returning=False):
+        if is_returning:
+            websockets.broadcast([self.opponent.websocket], {
+                'type': 'reconnect',
+                'id': self.id
+            })
+        else:
+            websockets.broadcast([self.opponent.websocket], {
+                'type': 'disconnect',
+                'id': self.id
+            })
+    
+    async def handle_disconnect(self):
+        if self.in_game:
+            self.notify_opponent()
+        await asyncio.sleep(3)
+        if self.active == False:
+            friends = self.get_friends()
+            websockets.broadcast(
+                [JOIN[id].websocket for id in friends.keys()], json.dumps({
+                    'type': 'logout',
+                    'id': self.id,
+                })
+            )
+            del JOIN[self.id]
+
+    def handle_reconnect(self, socket):
+        self.active = True
+        self.websocket = socket
+        if self.in_game:
+            self.notify_opponent(True)
+
+
 
 JOIN = {}
 CHAT = set()
@@ -45,79 +111,99 @@ async def handler(websocket, path):
 
     """
     CHAT.add(websocket)
+    user = 0
     # Receive and parse the "init" event from the UI.
-    async for message in websocket:
-        event = json.loads(message)
-        user_id = event["id"]
+    try:
+        async for message in websocket:
+            event = json.loads(message)
+            user_id = event["id"]
+            user = event["id"]
 
-        if event["type"] == 'message':
-            websockets.broadcast(CHAT, json.dumps(event))
-        print(event)
+            if event["type"] == 'message':
+                websockets.broadcast(CHAT, json.dumps(event))
 
-        if event["type"] == "accept":
-            print(JOIN)
-            await join_game(websocket, JOIN[event["id"]], JOIN[event["opponent"]], event)
+            if event["type"] == "accept":
+                await join_game(websocket, JOIN[event["id"]], JOIN[event["opponent"]], event)
 
 
-        elif event['type'] == 'invite':
-            player_1 = JOIN[event["id"]]
-            player_2 = JOIN[event["target"]]
-            await join_game(websocket, player_1, player_2, event)
-        
-        elif event['type'] == 'get_friends':
-            print(get_friends(event['ids']))
-            websockets.broadcast({websocket}, json.dumps({
-                "type": "players",
-                "players": get_friends(event['ids']),
-            }))
-
-        elif event["type"] == "start":
-            # First player starts a new game.
-            socket = Connection(event['id'], websocket)
-            JOIN[socket.id] = socket
-
-        elif event['type'] == "chatMessage":
-            if event['recipient_id'] == 0:
-
-                websockets.broadcast([user.websocket for user in JOIN.values() if user.id != event['id']], json.dumps({
-                        'type': 'chatMessage',
-                        'sender': event['sender'],
-                        'recipient_id': event['recipient_id'],
-                        'content': event['content'],
-                        'sender_id': event['id']
-                    }))
-            else:
-                sockets = {websocket}
-                if event['recipient_id'] in JOIN.keys():
-                    sockets = {websocket, JOIN[event['recipient_id']].websocket}
-                websockets.broadcast(sockets, json.dumps({
-                        'type': 'chatMessage',
-                        'sender': event['sender'],
-                        'recipient_id': event['recipient_id'],
-                        'content': event['content'],
-                        'sender_id': event['id']
-                    }))
-
-                    
-
-        elif event['type'] == 'status':
-            try:
-                user = JOIN[event['id']]
+            elif event['type'] == 'invite':
+                player_1 = JOIN[event["id"]]
+                player_2 = JOIN[event["target"]]
+                await join_game(websocket, player_1, player_2, event)
+            
+            elif event['type'] == 'get_friends':
                 websockets.broadcast({websocket}, json.dumps({
-                    'type': 'newFriend',
-                    'id': event['id'],
-                    'name': event['name'],
-                    'online': True,
-                    'in_game': user.in_game
+                    "type": "players",
+                    "players": get_friends(event['ids']),
                 }))
-            except: 
-                websockets.broadcast({websocket}, json.dumps({
-                    'type': 'newFriend',
-                    'id': event['id'],
-                    'name': event['name'],
-                    'online': False,
-                    'in_game': False
-                }))
+
+            elif event["type"] == "start":
+                # First player starts a new game.
+                if user in JOIN.keys():
+                    socket = JOIN[user]
+                    socket.active = True
+                    socket.handle_reconnect(websocket)
+                else:       
+                    socket = Connection(event['id'], websocket)
+                    JOIN[socket.id] = socket
+                    socket.handle_login()
+
+                websockets.broadcast(
+                    {websocket},
+                    json.dumps(
+                        {'type': 'friends', 'friends': socket.get_friends()}
+                    )
+                )
+                
+
+            elif event['type'] == "chatMessage":
+                if event['recipient_id'] == 0:
+
+                    websockets.broadcast([user.websocket for user in JOIN.values() if user.id != event['id']], json.dumps({
+                            'type': 'chatMessage',
+                            'sender': event['sender'],
+                            'recipient_id': event['recipient_id'],
+                            'content': event['content'],
+                            'sender_id': event['id']
+                        }))
+                else:
+                    sockets = {websocket}
+                    if event['recipient_id'] in JOIN.keys():
+                        sockets = {websocket, JOIN[event['recipient_id']].websocket}
+                    websockets.broadcast(sockets, json.dumps({
+                            'type': 'chatMessage',
+                            'sender': event['sender'],
+                            'recipient_id': event['recipient_id'],
+                            'content': event['content'],
+                            'sender_id': event['id']
+                        }))
+
+                        
+
+            elif event['type'] == 'status':
+                try:
+                    user = JOIN[event['id']]
+                    websockets.broadcast({websocket}, json.dumps({
+                        'type': 'newFriend',
+                        'id': event['id'],
+                        'name': event['name'],
+                        'online': True,
+                        'in_game': user.in_game
+                    }))
+                except: 
+                    websockets.broadcast({websocket}, json.dumps({
+                        'type': 'newFriend',
+                        'id': event['id'],
+                        'name': event['name'],
+                        'online': False,
+                        'in_game': False
+                    }))
+    finally: 
+        JOIN[user].active = False
+        await JOIN[user].handle_disconnect()
+        print(JOIN)
+
+
 
         
 async def join_game(websocket, player_1, player_2, event):
@@ -134,16 +220,13 @@ async def join_game(websocket, player_1, player_2, event):
             player_2
         )
     finally:
+
         player_1.in_game = False
         player_1.opponent = None
         player_2.in_game = False
         player_2.opponent = None 
 
 def get_friends(ids): 
-    print(ids)
-    print(JOIN.keys())
-    for id in ids:
-        print(id)
     return {JOIN[id].id: {"id": JOIN[id].id, "in_game": JOIN[id].in_game} for id in ids if id in JOIN.keys()}
 
 if __name__ == "__main__":
